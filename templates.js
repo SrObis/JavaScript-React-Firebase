3c6fb9851909b351ef5f5c243da1511052588f00
SrObis <obis2001@gmail.com>
2021-03-16 12:35:32 +0100

    template  strings

---
+++ templates.js
@@ -0,0 +1,5 @@
+const nombre = 'Jordi'
+const apellido = 'obis'
+console.log('El nomre es '+nombre+' y el apellido es '+apellido)
+console.log(`El nomre es ${nombre} y el apellido es ${apellido}`)
+//las templates string son los acentos si clickas dos veces te salen dos
