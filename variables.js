7da13a4238fd8aa4ec2849a2181cce78e5e6a649
SrObis <obis2001@gmail.com>
2021-03-16 12:26:31 +0100

    declaracion de variables

---
+++ variables.js
@@ -0,0 +1,30 @@
+/*
+    Numero = Number
+    Texto = String
+    Arrays = coleciones [1,2,3,4,...] 
+    Objetos {
+        Nombre: 'Jordi',
+        Apellido: 'Obis'
+    }
+    bollean -> true/false
+
+
+    var para declarar pero esta en desuso 
+*/
+
+
+let nombre = 'Jordi' 
+nombre = 'edad' //la variable nombre es mutable
+const edad = 20 //la variable edad no es mutable porque se usa el const
+// edad = 19  //si se intenta modficar una constante marca error
+const colecion = [1,2,3,4,5]
+const objeto = {
+        Nombre: 'Jordi',
+        Apellido: 'Obis'
+        //apellido key
+        //obis value
+
+}
+
+
+console.log(nombre,edad,colecion,objeto)//para mostrar por consola
