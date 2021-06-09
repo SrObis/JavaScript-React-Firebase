3589825a13b52488ff144de9540a0ed2e7b35ac2
jobis <obis2001@gmail.com>
2021-06-09 13:37:57 +0200

    12. Componentes
    Creación e implementación de un primer componente de jsx.

---
+++ primerproyecto/src/components/Hola.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Hola(){
+    return(
+        <div>
+            <h2>Primer componente "Hola"</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, tenetur dignissimos voluptatibus, repellendus asperiores veniam porro fugiat minima voluptate voluptas nihil facere itaque sit. Sapiente dicta totam eveniet quam. Facere?</p>
+        </div>
+    )
+}
+
+export default Hola
