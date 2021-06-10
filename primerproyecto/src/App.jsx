7444bc6b03f98ab1d2b9f21960df350eec239340
jobis <obis2001@gmail.com>
2021-06-10 10:39:18 +0200

    List key map
    Creación de una lista.

--- primerproyecto/src/App.jsx
+++ primerproyecto/src/App.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import './App.css';
 import Contador from './components/Contador';
+import Listado from './components/Listado'
 
 function App() {
   return (
     <div className="App">
       <h1>Titulo de nuestro primer proyecto</h1>
       {/* Cada componente contador es aparte no estan conectados entre si y el numero son todos distintos */}
-      <Contador></Contador>
-      <Contador></Contador>
-      <Contador></Contador>
-      <Contador></Contador>
+      <Listado></Listado>
     </div>
   );
 }
