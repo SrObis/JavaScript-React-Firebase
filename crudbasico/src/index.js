f48924c3cad1f314cac3ab1a264ee90b92d02aad
jobis <obis2001@gmail.com>
2021-06-11 13:36:59 +0200

    23. Instalación
    Instalacion de bootstrap y uniqid

--- crudbasico/src/index.js
+++ crudbasico/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,4 +9,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
