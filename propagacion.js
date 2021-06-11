03891e5bf64565b99bb874815c02838ad8b2d246
jobis <obis2001@gmail.com>
2021-06-11 12:13:34 +0200

    22. Operador de propagación
    Unir arrays

---
+++ propagacion.js
@@ -0,0 +1,5 @@
+const array1 = [1,2,3]
+const array2 = [4,5,6]
+
+const array3 = [...array1,...array2]//el operador de propagacion sirbe para unir arras sin eliminar las arrays juntadas
+console.log(array3)
