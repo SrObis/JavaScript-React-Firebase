91b05ecf901053c678264907ea4834e5d527cb78
jobis <obis2001@gmail.com>
2021-06-11 11:24:46 +0200

    21 Evento onSubmit
    Para comprobar que los campos no esten vacios

--- primerproyecto/src/components/Formulario.jsx
+++ primerproyecto/src/components/Formulario.jsx
@@ -4,11 +4,23 @@ const Formulario=()=>{
     const [nombre, setNombre] = useState('')
     const [edad, setEdad]= useState('')
 
+    const Validar = (event) =>{
+        event.preventDefault()//para evitar que se recarge la pantalla
+        console.log('Pulsado el boton')
+        if(!nombre.trim()){//trim compara la cadena de caracteres para ver si esta bacio
+            console.log('El nombre esta vacio')
+            return
+        }
+        if(!edad.trim()){
+            console.log('El campo edad esta vacio')
+            return
+        }
+    }
 
 
     return(
        <div className="container">
-           <form className="form-group">
+           <form onSubmit={Validar} className="form-group">
                <input 
                     placeholder="Introduce el nombre" 
                     className="form-control mb-3" 
