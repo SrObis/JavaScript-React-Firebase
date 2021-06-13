b979b0a269a4fa2cb6aa64e69749cf5c630d481d
jobis <obis2001@gmail.com>
2021-06-13 16:17:19 +0200

    25. Añadiendo Nombres

--- crudbasico/src/components/Listadonombres.jsx
+++ crudbasico/src/components/Listadonombres.jsx
@@ -1,7 +1,17 @@
-import React, {useStat} from 'react'
-
+import React, {useState} from 'react'
+import uniqid from 'uniqid'
 
 const Listadonombres = () =>{
+    const [nombre, setNombre] = useState('')
+    const [listanombres, setListaNombres]= useState([])
+    const addNombre = (e)=>{
+        e.preventDefault()
+        const nuevoNombre= {
+            id:uniqid(),
+            tituloNombre:nombre
+        }
+        setListaNombres([...listanombres,nuevoNombre])
+    }
     return(
         <div>
             <h2>Aplicacion CRUD BASICA</h2>
@@ -11,8 +21,8 @@ const Listadonombres = () =>{
                 </div>
                 <div className="col">
                     <h2>Formulario para añadir nombres</h2>
-                    <form className="form-group">
-                        <input className="form-control mb-3" type="text" placeholder="Introduce el nombre"/>
+                    <form onSubmit={(e)=> addNombre(e)} className="form-group">
+                        <input onChange={(e)=>setNombre(e.target.value)} className="form-control mb-3" type="text" placeholder="Introduce el nombre"/>
                         <input className="btn btn-info btn-block" type="submit" placeholder="Registrar Nombre"/>
 
                     </form>
