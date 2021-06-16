fe7c1c50322d9d4c18df3bf868d04d3b10fae118
jobis <obis2001@gmail.com>
2021-06-16 13:18:45 +0200

    35. Rutas uso de UseEffect

--- rutas/src/components/Base.jsx
+++ rutas/src/components/Base.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
 const Base = () =>{
+    const[nombre, setNombre] = useState('Pedro')
+
+    useEffect (()=>{/*El setTimeout se necesita un triger para ser lanzado con el useEffect cuando se haya rendereizado la pagina ejecutara el codigo*/
+        setTimeout(()=>{
+                setNombre('Jordi')
+        },2000)
+    })
+
     return (
         <div>
-            <h1>Pagina de base Ruta</h1>
+            <h1>Pagina de Base Ruta</h1>
+            {nombre}
         </div>
     )
 }
