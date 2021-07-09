8fe894ff87b8a17701121cdfed47d00b2cd1be0c
SrObis <obis2001@gmail.com>
2021-07-09 12:31:13 +0200

    40. Formulario instalación de firebase
    Creación del login y configurar la exportación de Firebase en el proyecto

--- crudbasico/src/components/Login.jsx
+++ crudbasico/src/components/Login.jsx
@@ -2,8 +2,28 @@ import React from 'react'
 
 const Login = () =>{
     return (
-        <div>
-            <h2>Login</h2>
+        <div className='row mt-5'>
+            <div className="col"></div>
+            <div className="col">
+                <form className='form-group'>
+                    <input
+                    className='form-control'
+                    placeholder='Introduce el Email'
+                    type='text'
+                    />
+                     <input
+                    className='form-control mt-4'
+                    placeholder='Introduce la Contraseña'
+                    type='password'
+                    /> 
+                    <input
+                    className='btn btn-dark btn-block mt-4'
+                    value='Registrar Usuario'
+                    type="submit"/>
+                </form>
+            </div>
+            <div className="col"></div>
+
         </div>
     )
 }
