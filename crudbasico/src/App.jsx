c521ff76c26844cbb0c162db635ec49620a26998
SrObis <obis2001@gmail.com>
2021-07-09 12:02:33 +0200

    39. Creación del Menu
    Creación de un Menú que redirige a los componentes, creación de los 4 componentes que se van a estar usando.

--- crudbasico/src/App.jsx
+++ crudbasico/src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import Liatadnombres from './components/Listadonombres'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Inicio from './components/Inicio'
+import Login  from './components/Login'
+import Admin from './components/Admin'
+import Menu from './components/Menu';
 
 function App() {
   return (
     <div className="container">
-      <Liatadnombres></Liatadnombres>
+      <Router>
+        <Menu></Menu>
+        <Switch>
+          <Route path='/admin' component={Admin}></Route>
+          <Route path='/login' component={Login}></Route>
+          <Route exact to='/' component={Inicio}></Route>
+
+        </Switch>
+      </Router>
     </div>
   );
 }
--- crudbasico/src/index.js
+++ crudbasico/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import bootstrap from 'bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.render(
   <React.StrictMode>
