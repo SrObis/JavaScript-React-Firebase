99ec5a1548b27545ca648b1973782fe31ae1e169
SrObis <obis2001@gmail.com>
2021-07-12 14:06:48 +0200

    47. Formulario
    Limpiar el proyecto, creación de un formulario con nombre y número de contacto

--- crudbasico/src/App.jsx
+++ crudbasico/src/App.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import Inicio from './components/Inicio'
-import Login  from './components/Login'
-import Admin from './components/Admin'
-import Menu from './components/Menu';
+
 
 function App() {
   return (
     <div className="container">
-      <Router>
-        <Menu></Menu>
-        <Switch>
-          <Route path='/admin' component={Admin}></Route>
-          <Route path='/login' component={Login}></Route>
-          <Route exact to='/' component={Inicio}></Route>
-
-        </Switch>
-      </Router>
+      <div className="row">
+        <div className="col">
+          <h2 className="d-flex justify-content-center">Formulario de Usuarios</h2>
+          <form className="form-group">
+            <input
+            className="form-control"
+            placeholder="Introduce un nombre"
+            type="text"
+            />
+            <input
+            className="form-control mt-3"
+            placeholder="Introduce un numero"
+            type="tel"
+            />
+            <input type="submit" value="Registrar" className="btn btn-dark btn-block mt-3"/>
+          </form>
+        </div>
+        <div className="col">
+          <h2 className="d-flex justify-content-center">Lista Contactos</h2>
+        </div>
+      </div>
     </div>
   );
 }
